9be4daaf900bbc215b5bbc7b82da8b9082d80a6a
Lowzy <65952016+Lowzy7530@users.noreply.github.com>
2026-02-23 22:09:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 # Copyright 2023-present Spicetify contributors.
 # Edited from project Denoland install script (https://github.com/denoland/deno_install)
 
-set -e
-
 for arg in "$@"; do
     shift
     case "$arg" in
